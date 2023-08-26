7e29a377b1da90710a89588ef6c05d0c344494b9
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-25 21:29:27 -0500

    upgrade button

--- Pgzero/Clicker/clicker.py
+++ Pgzero/Clicker/clicker.py
@@ -6,17 +6,23 @@ WIDTH = 800
 HEIGHT = 600
 TITLE = 'clicker'
 
+total_destruction = 0
+
 # actors
 bomb = Actor('bomb')
 bomb.x = WIDTH / 2
 bomb.y = HEIGHT / 2
+bomb.destruction = 1
 
-count = 0
+bomb_upgrade = Actor('buttonred', (100, 550))
+bomb_upgrade.disabled = False
 
 
 # draw function automatically called 60 times per second
 def draw():
     bomb.draw()
+    bomb_upgrade.draw()
+    screen.draw.text(f"Destruction: {total_destruction}", (20, 20))
 
 
 # gets called 60 times per second
@@ -26,9 +32,10 @@ def update():
 
 # only called when mouse is clicked
 def on_mouse_down(pos):
+    global total_destruction
     if bomb.collidepoint(pos):
         bomb.image = 'bombflash'
-
+        total_destruction += bomb.destruction
 
 def on_mouse_up(pos):
     if bomb.collidepoint(pos):
