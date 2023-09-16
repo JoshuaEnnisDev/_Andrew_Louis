ab8edf35f86cde038052668c723d0b3f942fb77d
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-09-15 21:31:16 -0500

    clicker goood enough

--- Pgzero/Clicker/clicker.py
+++ Pgzero/Clicker/clicker.py
@@ -6,32 +6,39 @@ WIDTH = 800
 HEIGHT = 600
 TITLE = 'WMD Clicker'
 
+# gloabal variables
 total_destruction = 0
+auto_value = 0
 
 # actors
 bomb = Actor('bomb')
 bomb.x = WIDTH / 2
 bomb.y = HEIGHT / 2
 bomb.destruction = 1
+bomb.cost = 10
 
 button_red = Actor('buttonred', (100, 550))
 button_red.cost = 10
 
+auto_button = Actor('buttonred', (200, 550))
+auto_button.value = 0
+
 
 # draw function automatically called 60 times per second
 def draw():
+    screen.fill((25, 103, 105))
     bomb.draw()
     button_red.draw()
-    screen.draw.text(f"Destruction: {total_destruction}", (20, 20))
-
-
-def reset_button(button, image):
-    button.image = image
+    auto_button.draw()
+    screen.draw.text(f"Destruction: {round(total_destruction)}", (20, 20))
+    screen.draw.text("Click Increase", center=(button_red.x, button_red.y + 10))
+    screen.draw.text(f"Cost: {button_red.cost}", center=(button_red.x, button_red.y + 20))
 
 
 # gets called 60 times per second
-def update():
-    screen.fill((25, 103, 105))
+def update(dt):
+    global total_destruction
+    total_destruction += auto_button.value * dt
 
 
 # only called when mouse is clicked
@@ -44,6 +51,11 @@ def on_mouse_down(pos):
     if button_red.collidepoint(pos) and total_destruction >= 10:
         button_red.image = "buttonred_pressed"
         bomb.destruction += 1
+        total_destruction -= 10
+
+    if auto_button.collidepoint(pos) and total_destruction >= 10:
+        auto_button.image = "buttonred_pressed"
+        auto_button.value += 1
         total_destruction -= 10
 
 
