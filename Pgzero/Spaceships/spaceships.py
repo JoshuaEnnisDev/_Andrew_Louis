a1d7bee7f120493791dd274c17ad223910fa0364
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-10-13 21:24:59 -0500

    commit

--- Pgzero/Spaceships/spaceships.py
+++ Pgzero/Spaceships/spaceships.py
@@ -6,6 +6,8 @@ WIDTH = 1000
 HEIGHT = 600
 TITLE = "Space Game"
 
+enemy_cooldown = 60  # 1 time per second
+
 # actors
 player = Actor("player", (500, 550))
 player.speed = 4
@@ -16,25 +18,26 @@ enemies = []
 # create 10 enemy actors
 for i in range(10):
     enemy = Actor("enemyblack1")
-    enemy.x = randint(50, 950)
+    enemy.cooldown = 60
+    enemy.x = i * 100 + 50
     # add enemy to the enemies list
     enemies.append(enemy)
 
 
-def draw():
-    screen.clear()
-    player.draw()
-    
-    # draw all enemies in the list
+def enemy_ai():
+
     for enemy in enemies:
-        enemy.draw()
-        
-    # draws all bullets in the list    
-    for bullet in player.bullets:
-        bullet.draw()
+        enemy.cooldown -= 1
+        if enemy.cooldown <= 0:
+            enemy.cooldown = 60
+            rand = randint(1, 2)
+            if rand == 1:
+                enemy.y += 50
+            elif rand == 2:
+                print("shoot a bullet")
 
 
-def update():
+def move_player():
     # move up
     if keyboard.w:
         player.y = player.y - player.speed
@@ -47,12 +50,30 @@ def update():
     # move down
     if keyboard.s:
         player.y += player.speed
+
+
+def draw():
+    screen.clear()
+    player.draw()
+
+    # draw all enemies in the list
+    for enemy in enemies:
+        enemy.draw()
+
+    # draws all bullets in the list
+    for bullet in player.bullets:
+        bullet.draw()
+
+
+def update():
+    move_player()
+    enemy_ai()
     # move the bullets
     for bullet in player.bullets:
         bullet.y -= player.bullet_speed
         if bullet.y <= 0:
             player.bullets.remove(bullet)
-    #------collisions------
+    # ------collisions------
     # bullet and enemy
     for enemy in enemies:
         if enemy.collidelist(player.bullets) != -1:
@@ -66,7 +87,7 @@ def on_key_down():
         bullet.angle = 90
         bullet.x = player.x
         bullet.y = player.top
-        # add bullet to listy=
+        # add bullet to list
         player.bullets.append(bullet)
 
 
