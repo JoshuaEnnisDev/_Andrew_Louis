025a343fe1323be2724d5d4eb74f5f19c12d0193
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-12-01 17:59:59 -0600

    commit

--- Pgzero/Physics/main.py
+++ Pgzero/Physics/main.py
@@ -1,16 +1,34 @@
 import pgzrun
+import math
 
 # x, y, width, height
+WIDTH = 800
+HEIGHT = 600
 
 shield_color = (70, 100, 120)
 
 player = Actor("ufoblue")
-shield = Rect(player.right, player.top, 50, 50)
+player.pos = (WIDTH // 2, HEIGHT // 2)
+shield = Rect(player.right, player.top, 10, 100)
+laser = Actor("laserblue13", anchor=("center", "center"))
 
 
 def draw():
-    screen.draw.filled_rect(shield, shield_color)
+    screen.clear()
+    #screen.draw.filled_rect(shield, shield_color)
+    #screen.draw.filled_rect(Rect(player.right, player.top, 10, 100), shield_color)
     player.draw()
+    laser.draw()
+
+
+def on_mouse_move(pos):
+    player.angle = player.angle_to(pos)
+    laser.center = (math.cos(player.right), math.sin(player.top))
+    print(player.topleft)
+
+def update():
+    pass
+   
 
 
 pgzrun.go()
