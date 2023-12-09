4c928a5e8890e7ce6e8bbcfc302932172e090fe8
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-12-08 21:08:31 -0600

    commit

--- Pgzero/tower-defense/main.py
+++ Pgzero/tower-defense/main.py
@@ -2,6 +2,8 @@ import pgzrun
 import random
 from pgzhelper import Actor
 
+# Bases everywhere but ther dirt path
+
 # Screen Setup
 WIDTH = 1920
 HEIGHT = 1080
@@ -9,12 +11,44 @@ TITLE = "Tower Defense"
 
 # Create actors
 floor = Actor("td1920x1080")
-base = Actor("base1")
+base = Actor("base1", (500, 200))
 
-# an empty list
-turrents = []
+# an empty list to hold turret actors
+turrets = []
 
 # a list with image names
 enemy_images = ["person1", "person2", "person3", "person4"]
+# an empty to hold enemy actors
+enemies = []
+
+
+# turret needs a range, bullet list
+def create_turret():
+    # create a turret actor
+    turret = Actor("turret_green")
+    turret.bullets = []
+    # add the turret actor to the turrets list
+    turrets.append(turret)
+
+
+# built in only runs when any mouse button is pressed (event handler)
+def on_mouse_down(pos):
+    print(pos)
+
+
+# draw function built in to pgzero
+def draw():
+    floor.draw()
+    base.draw()
+    for turret in turrets:
+        turret.draw()
+    for enemy in enemies:
+        enemy.draw()
+
+
+# main built in too pygame zero runs 60 a second
+def update():
+    pass
+
 
 pgzrun.go()
