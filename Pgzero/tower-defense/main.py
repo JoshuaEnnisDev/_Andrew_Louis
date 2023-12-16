e1b96c47f80545fdcb731d214dbf199e40bd7a3a
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-12-15 21:07:40 -0600

    got bases only off path

--- Pgzero/tower-defense/main.py
+++ Pgzero/tower-defense/main.py
@@ -11,7 +11,36 @@ TITLE = "Tower Defense"
 
 # Create actors
 floor = Actor("td1920x1080")
-base = Actor("base1", (500, 200))
+
+
+def is_on_path(x, y):
+    if 110 < x < 380 and 570 < y < 1080:
+        return True
+    if 350 < x < 800 and 570 < y < 800:
+        return True
+    if 570 < x < 820 and 120 < y < 570:
+        return True
+    if 830 < x < 1250 and 120 < y < 350:
+        return True
+    if 1000 < x < 1250 and 120 < y < 600:
+        return True
+    if 1250 < x < WIDTH and 350 < y < 600:
+        return True
+    
+    return False
+
+
+# create bases
+bases = []
+
+for row in range(0, 16):
+    for col in range(0, 30):
+        base = Actor("base1")
+        base.left = col * 64
+        base.top = row * 64
+        if not is_on_path(base.left, base.top):
+            bases.append(base)
+
 
 # an empty list to hold turret actors
 turrets = []
@@ -39,11 +68,12 @@ def on_mouse_down(pos):
 # draw function built in to pgzero
 def draw():
     floor.draw()
-    base.draw()
     for turret in turrets:
         turret.draw()
     for enemy in enemies:
         enemy.draw()
+    for base in bases:
+        base.draw()
 
 
 # main built in too pygame zero runs 60 a second
