23994d254b8a7a17d564bc37ce61243e2f6d01f4
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2024-01-19 20:20:20 -0600

    commit

---
+++ Pgzero/ChooseYourOwnAdventure/main.py
@@ -0,0 +1,13 @@
+
+
+# Data types
+    # String -- anything inside quotes 
+
+# function to display text on the screen
+print("Choose your own adventure")
+
+
+# function that allows the user to type into the program
+input("What is your name? ")
+
+
